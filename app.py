4ed0067205a8cff0b469abb3b24e694015491d11
leomlouzas <leonardomlouzas@gmail.com>
2024-02-25 13:35:35 -0300

    fix: Function names

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ def hello_world():
 
 # Routes
 @app.route('/hello')
-def hello_world():
+def new_route():
     return "<p>Hello, World!</p>"
 
 # Dynamic URLS
@@ -29,12 +29,13 @@ def parameters():
 
 # HTTP methods
 @app.route('/hello', methods=['GET', 'POST'])
-def greet():
+def hello():
     if request.method == 'GET':
         return "<p>You made a GET request!</p>"
     elif request.method == 'POST':
         return "<p>You made a POST request!</p>"
 
+# Status codes
 
 if __name__ == "__main__":
     app.run(debug=True)
