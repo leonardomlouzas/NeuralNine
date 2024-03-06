2d5f30b0d63500b085ae42fc4e9b2f5e84407159
leomlouzas <leonardomlouzas@gmail.com>
2024-03-05 21:00:27 -0300

    feat: Adds custom filters

--- app.py
+++ app.py
@@ -43,5 +43,26 @@ def filters():
     return render_template("filters.html", name=user_name)
 
 
+@app.route("/custom_filters")
+def custom_filters():
+    user_name = "Leonardo"
+    return render_template("custom_filters.html", name=user_name)
+
+
+@app.template_filter("reverse_string")
+def reverse_string(s):
+    return s[::-1]
+
+
+@app.template_filter("repeat")
+def repeat(s, times=2):
+    return s * times
+
+
+@app.template_filter("alternate_case")
+def alternate_case(s):
+    return "".join([c.upper() if i % 2 == 0 else c.lower() for i, c in enumerate(s)])
+
+
 if __name__ == "__main__":
     app.run()
