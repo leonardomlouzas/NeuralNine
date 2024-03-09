94a5b544204ffa6be47e365a357adec2c722019d
leomlouzas <leonardomlouzas@gmail.com>
2024-03-08 22:55:43 -0300

    feat: Adds file upload

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import pandas as pd
 from flask import Flask, render_template, request
 
 app = Flask(__name__, template_folder="templates")
@@ -17,6 +18,25 @@ def index():
 
         else:
             return "Failure."
+
+
+@app.route("/file_upload", methods=["GET", "POST"])
+def file_upload():
+    if request.method == "GET":
+        return render_template("file_upload.html")
+
+    if request.method == "POST":
+        file = request.files["file"]
+
+        if file.content_type == "text/plain":
+            return file.read().decode()
+
+        elif file.content_type in [
+            "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
+            "application/vnd.ms-excel",
+        ]:
+            df = pd.read_excel(file)
+            return df.to_html()
 
 
 if __name__ == "__main__":
