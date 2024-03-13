f0678f1510c2759b0e9505bfe9987c254a6365de
leomlouzas <leonardomlouzas@gmail.com>
2024-03-13 16:34:30 -0300

    feat: Adds session SET

--- app.py
+++ app.py
@@ -1,11 +1,20 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, session
 
 app = Flask(__name__, template_folder="templates")
+app.secret_key = "Secret Key"
 
 
 @app.route("/")
 def index():
     return render_template("index.html")
+
+
+@app.route("/session_set")
+def session_set():
+    session["username"] = "Leo"
+    session["message"] = "Hello, World"
+
+    return render_template("index.html", message="Session data set successfully")
 
 
 if __name__ == "__main__":
